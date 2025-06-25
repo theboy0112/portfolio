041326f10213aa484c7c12f32288b99062496759
theboy0112 <clifboycabrera12@gmail.com>
2025-06-25 23:16:22 +0800

    Initialize commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./home.jsx";
 import About from "./about.jsx";
 import Contact from "./contact.jsx";
-import Project from "./project.jsx";
-import Profile from "./profile.jsx";
+import Project from './project.jsx'
+import Profile from './profile.jsx'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+           <Route path="/project" element={<Project />} />
+            <Route path="/profile" element={<Profile />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
